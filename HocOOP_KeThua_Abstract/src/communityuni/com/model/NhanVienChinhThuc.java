557ff38e0e321e866599fd0b29d26e419dd08acb
ACER <ACER@KhoaNguyen>
2020-02-24 22:14:05 +0700

    Bai69_KeThuaAbstract

---
+++ HocOOP_KeThua_Abstract/src/communityuni/com/model/NhanVienChinhThuc.java
@@ -0,0 +1,22 @@
+package communityuni.com.model;
+
+public class NhanVienChinhThuc extends NhanVien {
+
+	public NhanVienChinhThuc() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NhanVienChinhThuc(int ma, String ten) {
+		super(ma, ten);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void tinhLuong() {
+		System.out.println("Đây là nhân viên chính thức");
+	}
+	
+
+	
+}
